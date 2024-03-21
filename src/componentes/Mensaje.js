3f2ef7321e0deff3dc274df4937118ac12fcdd80
Manuel Chaparro <meny.chap@gmail.com>
2024-03-20 19:18:52 -0600

    primer commit

---
+++ src/componentes/Mensaje.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Mensaje(){
+  return(
+    <>
+      <div>
+        <h1>Hola mundo</h1>
+      </div>
+
+      <div>
+        <p>Esta fue una practica de react</p>
+      </div>
+    </>
+    
+  )
+}
+export default Mensaje
